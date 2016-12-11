43b3de01295d3c5096014231a7b523d08a5ecfd9
DIWithers <d.ivy.withers@gmail.com>
2016-12-11 12:05:22 -0500

    needs validator

--- scripts.js
+++ scripts.js
@@ -214,4 +214,34 @@ var W = window.innerWidth, H = window.innerHeight;
    
     animate(); 
 ////---------end canvas(nodes)
+$(function () {
 
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+
+            var url = "mail.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+
+            $('#myModal').modal('show');
+            $('#contact-form')[0].reset();
+            return false;
+        }
+   
