6ee16094cb0010fe47d3b917605c99fa7611ce1f
DIWithers <d.ivy.withers@gmail.com>
2016-12-30 10:56:23 -0500

    small fix to mailer typo

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@
       // $mail->MsgHTML($_POST['company']."\n".$_POST['phone']."\n").$_POST['body']); //var must be the textarea from your form
       $mail->Body = "COMPANY: ".$_POST['company']."\n";
       $mail->Body .= "FULL NAME: ".$_POST['fullName']."\n";
-      $mail->Body .= "PHONE: "$._POST['phone']."\n";
+      $mail->Body .= "PHONE: ".$_POST['phone']."\n";
       $mail->Body .= "EMAIL: ".$_POST['email']."\n";
       $mail->Body .= "MESSAGE: ".$_POST['body'];
 
